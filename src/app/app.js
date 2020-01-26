43ea9bac1a31c4ae44a5eb0a4bb6f95120374fc3
Ayub <ayubshaikh1695@gmail.com>
2020-01-27 01:54:47 +0530

    removed default template that had links for learning react, added a heading element, gave it a class to center align the text, wrapped it within a parent app div and gave it a class name

---
+++ src/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <h2 className="text-align-center">Huddl App</h2>
+    </div>
+  );
+}
+
+export default App;
