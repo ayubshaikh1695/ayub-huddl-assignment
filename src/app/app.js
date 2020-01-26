7fd4ad796ae3dfe65024eb873da8c733256cbaea
Ayub <ayubshaikh1695@gmail.com>
2020-01-27 03:52:19 +0530

    added AllPosts to template

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './app.css';
+import AllPosts from './components/all-posts/all-posts';
 
 function App() {
   return (
     <div className="app-wrapper">
       <h2 className="text-align-center">Huddl Assignment</h2>
+      <AllPosts/>
     </div>
   );
 }
