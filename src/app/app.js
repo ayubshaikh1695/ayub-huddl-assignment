d02dcfc007fda0aaa0ab08425f232e4ea4b649c2
Ayub <ayubshaikh1695@gmail.com>
2020-01-29 01:49:30 +0530

    added Router for page routing, on root route, it will redirect to /posts

--- src/app/app.js
+++ src/app/app.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './app.css';
 import AllPosts from './components/all-posts/all-posts';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, Redirect
+} from "react-router-dom";
 
 function App() {
   return (
     <React.Fragment>
       <header className="header">
-        <h2 className="header-label">Test</h2>
+        <h2 className="header-label">Huddl Assignment</h2>
       </header>
-      <div className="app-wrapper bg-lavender">
-        <AllPosts />
-      </div>
+      <Router>
+        <div className="app-wrapper bg-lavender">
+          <Switch>
+            <Route exact path='/'>
+              <Redirect to="/posts"></Redirect>
+            </Route>
+            <Route path="/posts">
+              <AllPosts />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </React.Fragment>
   );
 }
