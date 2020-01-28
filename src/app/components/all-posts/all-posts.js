36565b8f4b16c0faf4cff5d567357ce5bb661036
Ayub Hossain <ayub@geektrust.in>
2020-01-28 18:41:33 +0530

    added logic to fetch users and posts data, generate the posts as lists

--- src/app/components/all-posts/all-posts.js
+++ src/app/components/all-posts/all-posts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './all-posts.css';
-import { getPosts } from '../../services/services'
+import { fetchUsers, fetchPosts } from '../../services/api-service'
 
 class AllPosts extends React.Component {
   constructor(props) {
@@ -9,21 +9,47 @@ class AllPosts extends React.Component {
   }
 
   componentDidMount() {
-    getPosts().then(res => {
-      const data = res.data;
-      this.setState({ posts: data });
-    }).catch(err => { })
+    fetchUsers().then(res => {
+      const users = res.data;
+      fetchPosts().then(res => {
+        const posts = res.data;
+        const postsList = posts.map(post => {
+          return {
+            title: post.title,
+            username: this.getUsername(users, post.userId)
+          }
+        })
+        this.setState({ posts: postsList });
+      }).catch(err => { });
+    }).catch(err => { });
+  }
+
+  getUsername(users, userId) {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id == userId) {
+        return users[i].username;
+      }
+    }
   }
 
   render() {
     const { posts } = this.state;
     return (
       <div className="all-posts-wrapper">
-        <ul>
-          {posts && posts.map((post, i) => {
-            return (<li key={i}>{post.title ? post.title : "N/A"}</li>)
-          })}
-        </ul>
+        {posts ? <ul className="posts-container">
+          {posts.map((post, i) => {
+            return <li key={i} className="post-list">
+              <div className="mr-12">
+                <i class="fas fa-user-circle user-icon"></i>
+              </div>
+              <div>
+                <div className="pri-txt mb-8">{post.title ? post.title : "N/A"}</div>
+                <div className="sec-txt user-name">{post.username ? post.username : "N/A"}</div>
+              </div>
+            </li>
+          })}</ul> : <div className="spinner-container">
+            <i class="fas fa-circle-notch fa-spin"></i>
+          </div>}
       </div>
     );
   }
