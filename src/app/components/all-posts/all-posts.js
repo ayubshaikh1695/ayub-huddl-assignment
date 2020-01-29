3841074f9e9fde34fb67d354c6443bcbf10d482b
Ayub Hossain <ayub@geektrust.in>
2020-01-30 00:54:46 +0530

    added logic to cancel api call if component is unmounted

--- src/app/components/all-posts/all-posts.js
+++ src/app/components/all-posts/all-posts.js
@@ -2,35 +2,59 @@ import React from 'react';
 import './all-posts.css';
 import { fetchUsers, fetchPosts } from '../../services/api-service';
 import Spinner from '../../shared/spinner/spinner';
+import axios from 'axios';
 
 class AllPosts extends React.Component {
+  signal = axios.CancelToken.source();
+  abortSignal = { cancelToken: this.signal.token };
+
   constructor(props) {
     super(props);
     this.state = { posts: null };
   }
 
   componentDidMount() {
-    fetchUsers().then(res => {
+    fetchUsers(this.abortSignal).then(res => {
       const users = res.data;
-      fetchPosts().then(res => {
+      fetchPosts(this.abortSignal).then(res => {
         const posts = res.data;
         const postsList = posts.map(post => {
           return {
             title: post.title,
-            username: this.getUsername(users, post.userId)
+            postId: post.id,
+            userData: this.getUserData(users, post.userId)
           }
         })
         this.setState({ posts: postsList });
-      }).catch(err => { });
-    }).catch(err => { });
+      }).catch(err => {
+        if (axios.isCancel(err)) {
+          console.log('Error: ', err.message);
+        }
+      });
+    }).catch(err => {
+      if (axios.isCancel(err)) {
+        console.log('Error: ', err.message);
+      }
+    });
   }
 
-  getUsername(users, userId) {
+  getUserData(users, userId) {
     for (let i = 0; i < users.length; i++) {
       if (users[i].id === userId) {
-        return users[i].username;
+        return {
+          username: users[i].username,
+          userId: users[i].id
+        };
       }
     }
+  }
+
+  navigateToPostRoute(userId, postId) {
+    this.props.history.push(`/post?userId=${userId}&postId=${postId}`);
+  }
+
+  componentWillUnmount() {
+    this.signal.cancel('Api is being canceled');
   }
 
   render() {
@@ -39,13 +63,13 @@ class AllPosts extends React.Component {
       <div className="all-posts-wrapper">
         {posts ? <ul className="posts-container">
           {posts.map((post, i) => {
-            return <li key={i} className="post-list">
+            return <li key={i} className="post-list" onClick={() => this.navigateToPostRoute(post.userData.userId, post.postId)}>
               <div className="mr-12">
                 <i className="fas fa-user-circle user-icon"></i>
               </div>
               <div>
                 <div className="pri-txt mb-4">{post.title ? post.title : "N/A"}</div>
-                <div className="sec-txt user-name">{post.username ? post.username : "N/A"}</div>
+                <div className="sec-txt user-name">{post.userData.username ? post.userData.username : "N/A"}</div>
               </div>
             </li>
           })}</ul> : <Spinner />}
--- src/app/components/post/post.js
+++ src/app/components/post/post.js
@@ -2,25 +2,30 @@ import React from 'react';
 import './post.css';
 import { fetchUserById, fetchPostById, fetchCommentsByPostId } from '../../services/api-service';
 import Spinner from '../../shared/spinner/spinner';
+import queryString from 'query-string';
+import axios from 'axios';
 
 class Post extends React.Component {
+  signal = axios.CancelToken.source();
+  abortSignal = { cancelToken: this.signal.token };
+
   constructor(props) {
     super(props);
     this.state = { postInfo: null };
   }
 
   componentDidMount() {
-    const userId = 1;
-    const postId = 1;
-    console.log(this.props);
+    const queryValues = queryString.parse(this.props.location.search);
+    const userId = queryValues.userId;
+    const postId = queryValues.postId;
 
-    fetchUserById(userId).then(res => {
+    fetchUserById(userId, this.abortSignal).then(res => {
       const user = res.data;
 
-      fetchPostById(postId).then(res => {
+      fetchPostById(postId, this.abortSignal).then(res => {
         const post = res.data;
 
-        fetchCommentsByPostId(postId).then(res => {
+        fetchCommentsByPostId(postId, this.abortSignal).then(res => {
           const comments = res.data;
 
           const data = {
@@ -29,10 +34,21 @@ class Post extends React.Component {
             comments: comments
           }
           this.setState({ postInfo: data });
-
-        }).catch(err => { });
-      }).catch(err => { });
-    }).catch(err => { });
+        }).catch(err => {
+          if (axios.isCancel(err)) {
+            console.log('Error: ', err.message);
+          }
+        });
+      }).catch(err => {
+        if (axios.isCancel(err)) {
+          console.log('Error: ', err.message);
+        }
+      });
+    }).catch(err => {
+      if (axios.isCancel(err)) {
+        console.log('Error: ', err.message);
+      }
+    });
   }
 
   getUsername(users, userId) {
@@ -43,27 +59,32 @@ class Post extends React.Component {
     }
   }
 
+  componentWillUnmount() {
+    this.signal.cancel('Api is being canceled');
+  }
+
   render() {
     const { postInfo } = this.state;
-    console.log(this.props);
     return (
       <div className="posts-wrapper">
         {postInfo ?
           <div className="post-container">
-            <div className="mr-12">
-              <i className="fas fa-user-circle user-icon"></i>
-            </div>
-            <div>
-              <div className="pri-txt mb-4">{postInfo.title ? postInfo.title : "N/A"}</div>
-              <div className="sec-txt user-name">{postInfo.username ? postInfo.username : "N/A"}</div>
+            <div className="post-detail mb-24 flex align-center">
+              <div className="mr-12">
+                <i className="fas fa-user-circle user-icon"></i>
+              </div>
+              <div>
+                <div className="pri-txt fw-600 mb-4">{postInfo.title ? postInfo.title : "N/A"}</div>
+                <div className="sec-txt user-name">{postInfo.username ? postInfo.username : "N/A"}</div>
+              </div>
             </div>
             {postInfo.comments && postInfo.comments.length > 0 ?
               <ul className="comments-container">
                 {postInfo.comments.map((comment, i) => {
-                  return <li key={i} className="comment-list">
-                    <div className="mb-4">
+                  return <li key={i} className="comment-list mb-16">
+                    <div className="mb-8">
                       <div className="pri-txt">{comment.name ? comment.name : "N/A"}</div>
-                      <div className="sec-txt user-name">{comment.email ? comment.email : "N/A"}</div>
+                      <div className="sec-txt">{comment.email ? comment.email : "N/A"}</div>
                     </div>
                     <div className="sec-txt">{comment.body ? comment.body : "N/A"}</div>
                   </li>
